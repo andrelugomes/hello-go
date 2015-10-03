3d6e03b3d950e1fa2565ed11281d13d74a1cccef
André Luis <andrelugomes@gmail.com>
2015-10-03 13:42:18 -0300

    First commit

---
+++ src/github.com/andrelugomes/hello/hello.go
@@ -0,0 +1,11 @@
+package main
+
+//github.com/andrelugomes/stringutil é um pacote gerado 
+import (
+		"fmt"
+		"github.com/andrelugomes/stringutil"
+)
+
+func main() {
+	fmt.Println(stringutil.Reverse("Hello World Go"))
+}
